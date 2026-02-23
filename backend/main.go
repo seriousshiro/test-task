fbcc4d63e83a8ff1d3f72b26142b221bf45e6aca
Павел Зибиров <shiro@MacBook-Air-Pavel.local>
2026-02-23 19:16:50 +0500

    Add health endpoint in golang app

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -10,6 +11,19 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Hello from Effective Mobile!"))
 	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	if len(os.Args) > 1 && os.Args[1] == "-healthcheck" {
+		resp, err := http.Get("http://localhost:8080/health")
+		if err != nil || resp.StatusCode != 200 {
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
 
 	log.Println("Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
